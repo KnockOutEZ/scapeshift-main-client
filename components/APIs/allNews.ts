d40202fab0acd8f71fdfe4dd4e0aefdf74e48e72
KnockOutEZ <70368615+KnockOutEZ@users.noreply.github.com>
2022-09-01 00:56:14 +0600

    news api changed

--- components/APIs/allNews.ts
+++ components/APIs/allNews.ts
@@ -1,33 +1,112 @@
 import axios from 'axios';
 export async function allNews() {
   var url =
-    "https://newsapi.org/v2/everything?" +
-    "q=bitcoin&" +
-    "from=2022-08-25&" +
-    "sortBy=popularity&" +
-    "apiKey=b5a5fc74e2864be48029772c874ce911";
+  `
+  http://api.mediastack.com/v1/news?access_key=b840fb58e435e544c84bef5e828fb060&countries=us,gb,de&limit=100
+  `;
 
-    const {data:res} = await axios.get(url);
-    console.log(res.articles)
-    return(res.articles)
+  let newRes={}
+  let final = []
+
+  function changeData(){
+
+    for(let i=0;i < res.data.length;i++){
+      newRes = {
+        author: res.data[i].author,
+        content: res.data[i].author,
+    description: res.data[i].description,
+    source: {name:res.data[i].source},
+    title: res.data[i].title,
+    url: res.data[i].url,
+    urlToImage:res.data[i].image
+      }
+      final.push(newRes)
+
+    }
+  }
+
+  const {data:res} = await axios.get(url)
+  console.log(res.data)
+  await changeData()
+  return final
+
+  // var url =
+  //   "https://newsapi.org/v2/everything?" +
+  //   "q=bitcoin&" +
+  //   "from=2022-08-25&" +
+  //   "sortBy=popularity&" +
+  //   "apiKey=b5a5fc74e2864be48029772c874ce911";
+
+  //   const {data:res} = await axios.get(url);
+  //   console.log(res.articles)
+  //   return(res.articles)
   }
 
 export async function trendingNews() {
-  var url ="https://newsapi.org/v2/top-headlines?country=us&apiKey=b5a5fc74e2864be48029772c874ce911"
-      const {data:res} = await axios.get(url);
-      console.log(res.articles)
-      return(res.articles)
+  var url =
+  `
+  http://api.mediastack.com/v1/news?access_key=b840fb58e435e544c84bef5e828fb060&countries=us,gb,de&limit=100
+  `;
+
+  let newRes={}
+  let final = []
+
+  function changeData(){
+
+    for(let i=0;i < res.data.length;i++){
+      newRes = {
+        author: res.data[i].author,
+        content: res.data[i].author,
+    description: res.data[i].description,
+    source: {name:res.data[i].source},
+    title: res.data[i].title,
+    url: res.data[i].url,
+    urlToImage:res.data[i].image
+      }
+      final.push(newRes)
+
+    }
+  }
+
+  const {data:res} = await axios.get(url)
+  console.log(res.data)
+  await changeData()
+  return final
+
+  // var url ="https://newsapi.org/v2/top-headlines?country=us&apiKey=b5a5fc74e2864be48029772c874ce911"
+  //     const {data:res} = await axios.get(url);
+  //     console.log(res.articles)
+  //     return(res.articles)
 }
 
 export async function weekly() {
   var url =
-    "https://newsapi.org/v2/everything?" +
-    "q=bitcoin&" +
-    "from=2022-08-29&" +
-    "sortBy=popularity&" +
-    "apiKey=b5a5fc74e2864be48029772c874ce911";
+  `
+  http://api.mediastack.com/v1/news?access_key=b840fb58e435e544c84bef5e828fb060&countries=us,gb,de&limit=100
+  `;
 
-    const {data:res} = await axios.get(url);
-    console.log(res.articles)
-    return(res.articles)
+  let newRes={}
+  let final = []
+
+  function changeData(){
+
+    for(let i=0;i < res.data.length;i++){
+      newRes = {
+        author: res.data[i].author,
+        content: res.data[i].author,
+    description: res.data[i].description,
+    source: {name:res.data[i].source},
+    title: res.data[i].title,
+    url: res.data[i].url,
+    urlToImage:res.data[i].image
+      }
+      final.push(newRes)
+
+    }
+  }
+
+  const {data:res} = await axios.get(url)
+  console.log(res.data)
+  await changeData()
+  return final
   }
